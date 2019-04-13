8ddf0fc115f7c90f0d0f601913af6579aee8fde8
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-04-13 22:31:23 +0300

    cw13

--- src/tests/users.spec.js
+++ src/tests/users.spec.js
@@ -1,45 +1,45 @@
-import chai from 'chai';
-import sinon from 'sinon';
-import faker from 'faker';
-import { getUsers } from './users';
+// import chai from 'chai';
+// import sinon from 'sinon';
+// import faker from 'faker';
+// import { getUsers } from './users';
 
-const { assert, expect } = chai;
-chai.should();
+// const { assert, expect } = chai;
+// chai.should();
 
-describe('getUsers()', () => {
-  let stub;
-  let fakeConsole;
-  const testString = faker.lorem.word();
+// describe('getUsers()', () => {
+//   let stub;
+//   let fakeConsole;
+//   const testString = faker.lorem.word();
 
-  beforeEach(() => {
-    stub = sinon.stub(window, 'fetch');
-    stub.returns(Promise.resolve({
-      json() { return Promise.resolve(testString); }
-    }));
-    fakeConsole = sinon.stub(window.console, 'log');
-  });
+//   beforeEach(() => {
+//     stub = sinon.stub(window, 'fetch');
+//     stub.returns(Promise.resolve({
+//       json() { return Promise.resolve(testString); }
+//     }));
+//     fakeConsole = sinon.stub(window.console, 'log');
+//   });
 
-  const createStub = () => {
-    const stub = sinon.stub(window, 'fetch');
-  }
+//   const createStub = () => {
+//     const stub = sinon.stub(window, 'fetch');
+//   }
 
-  afterEach(() => {
-    window.fetch.restore();
-    window.console.log.restore();
-  });
+//   afterEach(() => {
+//     window.fetch.restore();
+//     window.console.log.restore();
+//   });
 
-  it('should call fetch()', () => {
-    getUsers();
-    expect(stub.called).to.be.true;
-  });
+//   it('should call fetch()', () => {
+//     getUsers();
+//     expect(stub.called).to.be.true;
+//   });
 
-  it('should call console.log with result if success', (done) => {
-    const fakeConsole = sinon.stub(console, 'log');
+//   it('should call console.log with result if success', (done) => {
+//     const fakeConsole = sinon.stub(console, 'log');
 
-    // createStub;()
-    getUsers().then(() => {  
-      expect(fakeConsole.getCall(0).args[0]).to.equal(testString);
-      done();  
-    });
-  });
-});
+//     // createStub;()
+//     getUsers().then(() => {  
+//       expect(fakeConsole.getCall(0).args[0]).to.equal(testString);
+//       done();  
+//     });
+//   });
+// });
