a4e7f0c25fbe7efb95f7fc001d20a76e3efcc97f
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-04-13 23:12:52 +0300

    showMassege tested

---
+++ src/tests/main.spec.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import faker from 'faker';
+import { showMessage, getDay, getAdultUsers, getRandomUsers, Product } from './main';
+
+const { assert, expect } = chai;
+chai.should();
+
+describe('main', () => {
+  let product, name, fakeAlert;
+
+  beforeEach(() => {
+    product = new Product();
+    name = faker.name.firstName;
+  });
+// ------ showMessage ---------//
+  it('should alert with showMessage("text") was called', () => {
+    fakeAlert = sinon.stub(window, 'alert');
+    showMessage();
+    expect(fakeAlert.called).to.be.true;
+    window.alert.restore();
+  });
+  
+  it('should alert "text" with showMessage("text")', () => {
+    fakeAlert = sinon.stub(window, 'alert');
+    showMessage(name);
+    expect(fakeAlert.getCall(0).args[0]).to.equal(name);
+    window.alert.restore();
+  });
+
+});
