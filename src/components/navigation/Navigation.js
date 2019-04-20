2cb01dcd99af42b37d4a91ba2a4324f574b1a331
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-04-20 23:00:47 +0300

    task 1 done

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,19 @@
 import './navigation.scss';
 
+const list = ['Home', 'Products', 'Contacts'];
+
+const Nav = ({ list }) => (
+  <ul>
+    {list.map((item, i) => (
+      <li key={i}>
+        <a href={`./${item.toLowerCase()}`}>{item}</a>
+      </li>
+    ))}
+  </ul>
+);
+
 export const Navigation = () => (
   <nav className="nav nav_appearance">
-    <h2>Navigation</h2>
+    <Nav list={list} />
   </nav>
 );
