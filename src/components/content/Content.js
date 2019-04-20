3bf21b0010902c8ca583547d83fe1f9700b80600
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-04-20 23:40:16 +0300

    task 2 done

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,11 +1,45 @@
 import './content.scss';
 
+const now = new Date().getHours();
+let text = 'Hello';
+
+if (now >= 22 || now < 3) {
+  text = 'Good night';
+}
+if (now >= 3 && now < 12) {
+  text = 'Good morning';
+}
+if (now >= 12 && now < 18) {
+  text = 'Good afternoon';
+}
+if (now >= 18 && now < 22) {
+  text = 'Good evening';
+}
+
+const Greeting = ({ text, name }) => {
+  if (name) {
+    return (
+      <p>
+        {`${text}, ${name}`}
+
+!
+      </p>
+    );
+  }
+  return (
+    <p>
+      {text}
+
+!
+    </p>
+  );
+};
+
 export const Content = () => (
   <section className="content content_appearence">
     <h2 className="content__title">main</h2>
     <div className="content__desc">
-
-      Lorem ipsum dolor sit amet consectetur
+      <Greeting text={text} />
     </div>
   </section>
 );
