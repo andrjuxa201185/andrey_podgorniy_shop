41ea277a1b16f09afbc603031f1b3560d014368f
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-04-25 22:08:25 +0300

    task 2 done

--- src/components/show/Show.js
+++ src/components/show/Show.js
@@ -2,18 +2,23 @@ import './show.scss';
 
 export class Show extends Component {
   state = {
-    btnShow: 'btn-show',
-    show: 'show-content'
+    btnText: 'Show',
+    show: ''
   }
 
-  showContent(event) {
-    event.target.textContent = 'Hide';
+  showContent = () => {
+    const { btnText, show } = this.state;
+
+    this.setState({
+      btnText: btnText === 'Show' ? 'Hide' : 'Show',
+      show: show === '' ? 'show-content' : ''
+    });
   }
 
   render() {
     return (
       <div className="b-show">
-        <button className={this.state.btnShow} onClick={this.showContent}>Show</button>
+        <button className={this.state.show} onClick={this.showContent}>{this.state.btnText}</button>
         <div className={`content ${this.state.show}`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Lorem ipsum dolor sit amet consectetur adipisicing elit
