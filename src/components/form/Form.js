fa64335be52d00905e38295715a349ab3e6e6874
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-05-04 23:20:38 +0300

    cw19

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,118 +2,131 @@ import { Component } from 'react';
 import './form.scss';
 
 export class Form extends Component {
-  // fields = [
-  //   { label: 'email', reg: /^\w+@\w+\.[a-z]{2,}$/ },
-  //   { label: 'name', reg: /^[^ ]{3,20}$/ },
-  //   { label: 'surname', reg: /^[^ ]{3,20}$/ },
-  //   { label: 'password', reg: /^[^ ]{6,20}$/, secure: true }
-  // ];
+  fields = [
+    { label: 'email', reg: /^\w+@\w+\.[a-z]{2,}$/ },
+    { label: 'name', reg: /^[^ ]{3,20}$/ },
+    { label: 'surname', reg: /^[^ ]{3,20}$/ },
+    { label: 'password', reg: /^[^ ]{6,20}$/, secure: true }
+  ];
 
-  // state = this.fields.reduce((acc, item) => ({ ...acc, [item.label]: { value: '', error: '' } }), {});
+  state = this.fields.reduce((acc, item) => ({ ...acc, [item.label]: { value: '', error: '' } }), {});
 
   changeField = ({ target }) => {
-    const value = target.hasOwnProperty('checked') ? target.checked : target.value;
+    const value = Object.prototype.hasOwnProperty.call(target, 'checked') ? target.checked : target.value;
 
     this.setState({ [target.name]: { value, error: '' } });
   }
 
-  // validateField = ({ target }, index) => {
-  //   const field = this.fields[index];
-  //   const stateField = this.state[field.label];
+  validateField = (index) => {
+    const field = this.fields[index];
+    // eslint-disable-next-line react/destructuring-assignment
+    const stateField = this.state[field.label];
 
-  //   if (stateField.value.length === 0) {
-  //     this.setState({
-  //       [field.label]: { ...stateField, error: 'This field if wrong' }
-  //     });
-  //   }
-  // }
+    if (stateField.value.length === 0) {
+      this.setState({
+        [field.label]: { ...stateField, error: 'This field is required' }
+      });
 
-  // onSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log(this.state);
-  // }
+      return;
+    }
 
-  // getDisabledState() {
-  //   return Object.values(this.state).some(state => !state.value || state.error);
-  // }
-
-  // render() {
-  //   return (
-  //     <form onSubmit={this.onSubmit}>
-  //       {
-  //         this.fields.map(({ label, secure }, index) => {
-  //           // const sta
-  //           return (
-  //             <p key={label}>
-  //               <input
-  //                 type={secure ? 'password' : 'text'}
-  //                 name={label}
-  //                 placeholder={`Enter a ${label}`}
-  //                 value={this.state[label].value}
-  //                 onChange={this.changeField}
-  //                 onBlur={e => this.validateField(e, index)}
-  //                 className={this.state.error ? 'error' : 'correct'}
-  //               />
-  //               {this.state[label].error && <mark>strhnd</mark>}
-  //             </p>
-  //           );
-  //         })
-  //       }
-
-  //       <input type="submit" disabled={this.getDisabledState} value="Save" />
-  //     </form>
-  //   );
-  // }
-
-  state = {
-    name: '',
-    age: undefined,
-    role: '',
-    gender: true,
+    if (!field.reg.test(stateField.value)) {
+      this.setState({
+        [field.label]: { ...stateField, error: 'This field is wrong' }
+      });
+    }
   }
 
-  roles = ['', 'Admin', 'Guest', 'Unknown'];
+  onSubmit = (event) => {
+    event.preventDefault();
+    window.console.log(this.state);
+  }
+
+  getDisabledState() {
+    return Object.values(this.state).some(state => !state.value || state.error);
+  }
 
   render() {
-    const {
-      name, age, role, gender
-    } = this.state;
+    const { disabledFields = [] } = this.props;
 
     return (
-      <form action="">
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={this.changeField}
-        />
+      <form className="form" onSubmit={this.onSubmit}>
+        {
+          this.fields.map(({ label, secure }, index) => {
+            // eslint-disable-next-line react/destructuring-assignment
+            const state = this.state[label];
 
-        <input
-          type="text"
-          name="age"
-          value={age}
-          onChange={this.changeField}
-        />
-
-        <select
-          name="role"
-          value={role}
-          onChange={this.changeField}
-        >
-          {
-            ['', ...this.roles.map(role => <option value={role}>{role}</option>)]
-          }
-        </select>
-
-        <input
-          type="checkbox"
-          name="gender"
-          checked={gender}
-          onChange={this.changeField}
-        />
-
-        <p>{name.toUpperCase()}{age}</p>
+            return (
+              <p key={label}>
+                <input
+                  type={secure ? 'password' : 'text'}
+                  name={label}
+                  placeholder={`Enter a ${label}`}
+                  value={state.value}
+                  onChange={this.changeField}
+                  onBlur={() => this.validateField(index)}
+                  className={state.error ? 'error' : 'correct'}
+                  disabled={disabledFields.includes(label)}
+                />
+                {state.error && <mark>{state.error}</mark>}
+              </p>
+            );
+          })
+        }
+        <input type="submit" value="Save" disabled={this.getDisabledState()} />
       </form>
     );
   }
+
+  // state = {
+  //   name: '',
+  //   age: undefined,
+  //   role: '',
+  //   gender: true,
+  // }
+
+  // roles = ['', 'Admin', 'Guest', 'Unknown'];
+
+  // render() {
+  //   const {
+  //     name, age, role, gender
+  //   } = this.state;
+
+  //   return (
+  //     <form action="">
+  //       <input
+  //         type="text"
+  //         name="name"
+  //         value={name}
+  //         onChange={this.changeField}
+  //       />
+
+  //       <input
+  //         type="text"
+  //         name="age"
+  //         value={age}
+  //         onChange={this.changeField}
+  //       />
+
+  //       <select
+  //         name="role"
+  //         value={role}
+  //         onChange={this.changeField}
+  //       >
+  //         {
+  //           ['', ...this.roles.map(role => <option key={role} value={role}>{role}</option>)]
+  //         }
+  //       </select>
+
+  //       <input
+  //         type="checkbox"
+  //         name="gender"
+  //         checked={gender}
+  //         onChange={this.changeField}
+  //       />
+
+  //       <p>{name.toUpperCase()}{age}{role}</p>
+  //     </form>
+  //   );
+  // }
 }
