10eb09e9136a8d2b7b775fa8eafdf66807a62e58
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-05-05 22:09:32 +0300

    task 1 done

---
+++ src/components/editableField/EditableField.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import './editableField.scss';
+
+export class EditableField extends Component {
+  state = {
+    isHidden: true,
+    value: 'input value...',
+  }
+
+  changeField = ({ target }) => {
+    this.setState({ value: target.value });
+  }
+
+  changeHidden = () => {
+    const { isHidden, value } = this.state;
+    const { fn } = this.props;
+    fn && fn(value);
+    this.setState({ isHidden: !isHidden });
+  }
+
+  render() {
+    const { value, isHidden } = this.state;
+
+    return (
+      <div className="editable-field">
+        <h3>EditableField</h3>
+        <input
+          className={isHidden ? 'hidden' : 'show'}
+          type="text"
+          onChange={this.changeField}
+          onBlur={this.changeHidden}
+        />
+        <span
+          className={isHidden ? 'show' : 'hidden'}
+          onClick={this.changeHidden}
+        >{value}
+        </span>
+      </div>
+    );
+  }
+}
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
 import './main.scss';
 import { Form } from '../form';
+import { EditableField } from '../editableField/EditableField';
 
 export const Main = () => (
   <main className="main main_appearence">
     <Form />
+    <EditableField fn={console.log} />
   </main>
 );
