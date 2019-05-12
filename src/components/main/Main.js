e52ce70ece01a6a7285414cac0b480dbfed91674
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-05-12 12:05:24 +0300

    remake Users class to Users hook

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,7 +3,6 @@ import './main.scss';
 import { Tabs, Tab } from '../tabs';
 import { Users } from '../users';
 import { Images } from '../images';
-
 // eslint-disable-next-line padded-blocks
 export const Main = () => {
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,32 +1,53 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './users.scss';
 
-export class Users extends Component {
-  state = {
-    users: []
-  }
+// export class Users extends Component {
+//   state = {
+//     users: []
+//   }
 
-  getUsers = () => {
+//   getUsers = () => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(resp => resp.json())
+//       .then(users => this.setState({ users }));
+//   }
+
+//   componentDidMount() {
+//     this.getUsers();
+//   }
+
+//   render() {
+//     const { users } = this.state;
+
+//     return (
+//       <ul className="users">
+//         {
+//           users.map(({ name, id }) => (
+//             <li key={id}>{name}</li>
+//           ))
+//         }
+//       </ul>
+//     );
+//   }
+// }
+
+export const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(resp => resp.json())
-      .then(users => this.setState({ users }));
-  }
+      .then(users => setUsers(users));
+  }, []);
 
-  componentDidMount() {
-    this.getUsers();
-  }
-
-  render() {
-    const { users } = this.state;
-
-    return (
-      <ul className="users">
-        {
-          users.map(({ name, id }) => (
-            <li key={id}>{name}</li>
-          ))
-        }
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="users">
+      {
+        users.map(({ name, id }) => (
+          <li key={id}>{name}</li>
+        ))
+      }
+    </ul>
+  );
+};
