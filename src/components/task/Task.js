53f271e8e505b72ccff64bb1f26dc1fc87b86eca
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-05-12 13:13:15 +0300

    add todo

---
+++ src/components/task/Task.js
@@ -0,0 +1,28 @@
+import './task.scss';
+
+export class Task extends Component {
+  state = {
+    userName: 'userName',
+    tasks: 10,
+    doneTasks: 3,
+    progressTasks: 5,
+    waitingTasks: 1,
+  }
+
+  render() {
+    const {
+      userName, tasks, doneTasks, progressTasks, waitingTasks
+    } = this.state;
+
+    return (
+      <div className="task">
+        <h3>Hello, {userName}</h3>
+        <p>You have {tasks} tasks</p>
+        <p>Done: {doneTasks}</p>
+        <p>In progress: {progressTasks}</p>
+        <p>Waiting: {waitingTasks}</p>
+        <a href="/">Go to the task list</a>
+      </div>
+    );
+  }
+}
--- src/components/tasklist/Tasklist.js
+++ src/components/tasklist/Tasklist.js
@@ -1,28 +1,9 @@
 import './tasklist.scss';
+import { useState, useEffect } from 'react';
+import { Tabs, Tab } from '../tabs';
 
-export class Tasklist extends Component {
-  state = {
-    userName: 'userName',
-    tasks: 10,
-    doneTasks: 3,
-    progressTasks: 5,
-    waitingTasks: 1,
-  }
-
-  render() {
-    const {
-      userName, tasks, doneTasks, progressTasks, waitingTasks
-    } = this.state;
-
-    return (
-      <div className="tasklist">
-        <h3>Hello, {userName}</h3>
-        <p>You have {tasks} tasks</p>
-        <p>Done: {doneTasks}</p>
-        <p>In progress: {progressTasks}</p>
-        <p>Waiting: {waitingTasks}</p>
-        <a href="/">Go to the task list</a>
-      </div>
-    );
-  }
-}
+export const Tasklist = () => {
+  return (
+    <div>tasklist</div>
+  );
+};
