fa77047ebd73c54456a04b1425c1c02a0f796928
andrey.podgorniy <andrey.podgorniy@ideus.biz>
2019-06-04 09:38:14 +0300

    saga

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import { user } from './user';
 import { products, product } from './products';
 import { categories, info } from './categories';
 import { status } from './status';
-// import { rootSaga } from './rootSaga';
+import { rootSaga } from './rootSaga';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
@@ -25,4 +25,4 @@ export const store = createStore(
   composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
-// sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
